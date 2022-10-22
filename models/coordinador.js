91bebaf7fba653b5b19130a2ee2a1efdcf546516
ongorio <ongorio5@gmail.com>
2022-10-21 20:07:06 -0500

    corrected cordinadors

--- models/coordinador.js
+++ models/coordinador.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     }, {
         sequelize,
         modelName: 'Coordinador',
-        tableName: 'Coordinadores'
+        // tableName: 'Coordinadors'
     });
     return Coordinador;
 };
