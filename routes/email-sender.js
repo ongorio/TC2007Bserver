37ba6913379a7ae6b443f47a1756d0a0e1d38775
Marcelo91216 <A00831137@tec.mx>
2022-11-09 19:40:22 -0600

    contraseña como variable de entorno

--- routes/email-sender.js
+++ routes/email-sender.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
+const config = require('config');
 const CodeGenerator = require('node-code-generator');
 
 router.post("/code-section/", (req, res) => {    
     try{
         let generator = new CodeGenerator();
         let code = generator.generateCodes('######', 1, {});
+        if(!config.get('PASSWORD')){
+            console.log('For some reason, the password is not there...');
+            process.exit(1);
+        }
 
         const transporter = nodemailer.createTransport({
             host: 'smtp-mail.outlook.com',
@@ -16,7 +21,7 @@ router.post("/code-section/", (req, res) => {
             },
             auth:{
                 user: 'prepanet-oficial@hotmail.com',
-                pass:'------',
+                pass: config.get('PASSWORD'),
             }
         });
         const mailOptions = {
