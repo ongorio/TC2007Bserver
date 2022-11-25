4050ff7c350551908839ee290637161b740dff75
Marcelo91216 <A00831137@tec.mx>
2022-11-25 14:43:40 -0600

    inclui a marcelo

--- seeders/20221125200806-correoDeEmilio.js
+++ seeders/20221125200806-correoDeEmilio.js
@@ -16,10 +16,21 @@ module.exports = {
         isAlumno: true,
         createdAt: new Date(),
         updatedAt: new Date()
+      },{
+        email: "A00831137@tec.mx",
+        password: "XD",
+        first_name: "Marcelo",
+        last_name: "Todopoderoso",
+        isAdmin: false,
+        isCoord: false,
+        isAlumno: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ]);
 
-    const user = await sequelize.query("SELECT id FROM Users WHERE email='A01540484@tec.mx'", {type: sequelize.QueryTypes.SELECT});
+    const user1 = await sequelize.query("SELECT id FROM Users WHERE email='A01540484@tec.mx'", {type: sequelize.QueryTypes.SELECT});
+    const user2 = await sequelize.query("SELECT id FROM Users WHERE email='A00831137@tec.mx'", {type: sequelize.QueryTypes.SELECT});
 
     await queryInterface.bulkInsert('Alumnos', [
       {
@@ -27,7 +38,15 @@ module.exports = {
         birthDate: new Date(1999, 12, 1),
         campusId: 1,
         code: "900100",
-        userId: user[0].id,
+        userId: user1[0].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        matricula: "A00831137",
+        birthDate: new Date(2002, 2, 24),
+        campusId: 1,
+        code: "420690",
+        userId: user2[0].id,
         createdAt: new Date(),
         updatedAt: new Date()
       }
