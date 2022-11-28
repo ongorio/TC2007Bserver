29988a52b1ea3c39f847bc9ec11d6dc86d445100
ongorio <ongorio5@gmail.com>
2022-11-28 16:24:10 -0600

    corrected seeds

--- seeders/20221125200806-correoDeEmilio.js
+++ seeders/20221125200806-correoDeEmilio.js
@@ -1,14 +1,19 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
 const { sequelize } = require('../models');
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [
+
+    const salt = await bcrypt.genSalt();
+    const pass1 = await bcrypt.hash('1234', salt);
+    const pass2 = await bcrypt.hash('XD', salt)
+
+    queryInterface.bulkInsert('Users', [
       {
         email: "A01540484@tec.mx",
-        password: "1234",
+        password: pass1,
         first_name: "Emilio",
         last_name: "Perro",
         isAdmin: false,
@@ -18,7 +23,7 @@ module.exports = {
         updatedAt: new Date()
       },{
         email: "A00831137@tec.mx",
-        password: "XD",
+        password: pass2,
         first_name: "Marcelo",
         last_name: "Todopoderoso",
         isAdmin: false,
@@ -29,28 +34,7 @@ module.exports = {
       }
     ]);
 
-    const user1 = await sequelize.query('SELECT id FROM Users WHERE email="A01540484@tec.mx"', {type: sequelize.QueryTypes.SELECT});
-    const user2 = await sequelize.query('SELECT id FROM Users WHERE email="A00831137@tec.mx"', {type: sequelize.QueryTypes.SELECT});
-
-    await queryInterface.bulkInsert('Alumnos', [
-      {
-        matricula: "A01540484",
-        birthDate: new Date(1999, 12, 1),
-        campusId: 1,
-        code: "900100",
-        userId: user1[0].id,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },{
-        matricula: "A00831137",
-        birthDate: new Date(2002, 2, 24),
-        campusId: 1,
-        code: "420690",
-        userId: user2[0].id,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]);
+   
   },
 
   async down (queryInterface, Sequelize) {
---
+++ seeders/20221128032913-alumnosEmi.js
@@ -0,0 +1,38 @@
+'use strict';
+const { sequelize } = require('../models');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const user1 = await sequelize.query('SELECT id FROM Users WHERE email="A01540484@tec.mx"', {type: sequelize.QueryTypes.SELECT});
+    const user2 = await sequelize.query('SELECT id FROM Users WHERE email="A00831137@tec.mx"', {type: sequelize.QueryTypes.SELECT});
+
+    await queryInterface.bulkInsert('Alumnos', [
+      {
+        matricula: "A01540484",
+        birthDate: new Date(1999, 12, 1),
+        campusId: 1,
+        code: "900100",
+        userId: user1[0].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        matricula: "A00831137",
+        birthDate: new Date(2002, 2, 24),
+        campusId: 1,
+        code: "420690",
+        userId: user2[0].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
